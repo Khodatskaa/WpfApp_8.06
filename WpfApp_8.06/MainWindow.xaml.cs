50ca19924cd4b09ae906f0ff7585e8eb8e8b035d
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-08 18:32:32 +0300

    Ex 4 was done

--- WpfApp_8.06/MainWindow.xaml.cs
+++ WpfApp_8.06/MainWindow.xaml.cs
@@ -1,13 +1,9 @@
-﻿using System.Text;
+﻿using Microsoft.Win32;
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp_8._06
 {
@@ -16,6 +12,93 @@ namespace WpfApp_8._06
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void BrowseSourceDirectory_Click(object sender, RoutedEventArgs e)
+        {
+            var openFileDialog = new OpenFileDialog();
+            openFileDialog.ValidateNames = false;
+            openFileDialog.CheckFileExists = false;
+            openFileDialog.CheckPathExists = true;
+            openFileDialog.FileName = "Select Folder";
+
+            if (openFileDialog.ShowDialog() == true)
+            {
+                string path = Path.GetDirectoryName(openFileDialog.FileName);
+                SourceDirectoryPathTextBox.Text = path;
+            }
+        }
+
+        private void BrowseDestination_Click(object sender, RoutedEventArgs e)
+        {
+            var folderDialog = new SaveFileDialog();
+            folderDialog.ValidateNames = false;
+            folderDialog.CheckFileExists = false;
+            folderDialog.CheckPathExists = true;
+            folderDialog.FileName = "Select Folder";
+
+            if (folderDialog.ShowDialog() == true)
+            {
+                string path = Path.GetDirectoryName(folderDialog.FileName);
+                DestinationPathTextBox.Text = path;
+            }
+        }
+
+        private async void StartCopy_Click(object sender, RoutedEventArgs e)
+        {
+            string sourceDirectoryPath = SourceDirectoryPathTextBox.Text;
+            string destinationPath = DestinationPathTextBox.Text;
+            int numThreads = int.Parse(((ComboBoxItem)ThreadsComboBox.SelectedItem).Content.ToString());
+
+            if (!Directory.Exists(sourceDirectoryPath))
+            {
+                MessageBox.Show("Source directory does not exist!");
+                return;
+            }
+
+            try
+            {
+                await Task.Run(() => CopyDirectory(sourceDirectoryPath, destinationPath, numThreads));
+                MessageBox.Show("Directory copied successfully!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}");
+            }
+        }
+
+        private void CopyDirectory(string sourceDirectoryPath, string destinationPath, int numThreads)
+        {
+            var sourceDirectory = new DirectoryInfo(sourceDirectoryPath);
+            var destinationDirectory = new DirectoryInfo(destinationPath);
+
+            if (!destinationDirectory.Exists)
+            {
+                destinationDirectory.Create();
+            }
+
+            var files = sourceDirectory.GetFiles();
+            Parallel.ForEach(files, new ParallelOptions { MaxDegreeOfParallelism = numThreads }, file =>
+            {
+                string filePath = Path.Combine(destinationPath, file.Name);
+                file.CopyTo(filePath, true);
+                UpdateProgressBar(1.0 / files.Length);
+            });
+
+            var directories = sourceDirectory.GetDirectories();
+            foreach (var directory in directories)
+            {
+                string directoryPath = Path.Combine(destinationPath, directory.Name);
+                CopyDirectory(directory.FullName, directoryPath, numThreads);
+            }
+        }
+
+        private void UpdateProgressBar(double value)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                ProgressBar.Value += value * 100;
+            });
         }
     }
 }
