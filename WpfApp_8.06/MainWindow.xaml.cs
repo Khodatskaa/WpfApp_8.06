4fc71874e245e867652af73c5c5d229c5ce0cc28
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-08 18:13:55 +0300

    Ex 1 was done

--- WpfApp_8.06/MainWindow.xaml.cs
+++ WpfApp_8.06/MainWindow.xaml.cs
@@ -11,14 +11,102 @@ using System.Windows.Shapes;
 
 namespace WpfApp_8._06
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private Thread numberThread;
+        private Thread letterThread;
+        private Thread symbolThread;
+        private bool isRunning;  
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            isRunning = true;
+
+            numberThread = new Thread(GenerateNumbers)
+            {
+                Priority = GetPriority(NumberPriorityComboBox.Text)
+            };
+
+            letterThread = new Thread(GenerateLetters)
+            {
+                Priority = GetPriority(LetterPriorityComboBox.Text)
+            };
+
+            symbolThread = new Thread(GenerateSymbols)
+            {
+                Priority = GetPriority(SymbolPriorityComboBox.Text)
+            };
+
+            numberThread.Start();
+            letterThread.Start();
+            symbolThread.Start();
+
+            StartButton.IsEnabled = false;
+            StopButton.IsEnabled = true;
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            isRunning = false;
+
+            numberThread?.Join();
+            letterThread?.Join();
+            symbolThread?.Join();
+
+            StartButton.IsEnabled = true;
+            StopButton.IsEnabled = false;
+        }
+
+        private void GenerateNumbers()
+        {
+            Random random = new Random();
+            while (isRunning)
+            {
+                int number = random.Next(0, 100);
+                Dispatcher.Invoke(() => OutputTextBox.AppendText($"{number} "));
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void GenerateLetters()
+        {
+            Random random = new Random();
+            while (isRunning)
+            {
+                char letter = (char)random.Next('A', 'Z' + 1);
+                Dispatcher.Invoke(() => OutputTextBox.AppendText($"{letter} "));
+                Thread.Sleep(1000);
+            }
+        }
+
+        private void GenerateSymbols()
+        {
+            char[] symbols = { '!', '@', '#', '$', '%', '^', '&', '*', '(', ')' };
+            Random random = new Random();
+            while (isRunning)
+            {
+                char symbol = symbols[random.Next(symbols.Length)];
+                Dispatcher.Invoke(() => OutputTextBox.AppendText($"{symbol} "));
+                Thread.Sleep(1000);
+            }
+        }
+
+        private ThreadPriority GetPriority(string priority)
+        {
+            return priority switch
+            {
+                "Lowest" => ThreadPriority.Lowest,
+                "BelowNormal" => ThreadPriority.BelowNormal,
+                "Normal" => ThreadPriority.Normal,
+                "AboveNormal" => ThreadPriority.AboveNormal,
+                "Highest" => ThreadPriority.Highest,
+                _ => ThreadPriority.Normal
+            };
         }
     }
 }
