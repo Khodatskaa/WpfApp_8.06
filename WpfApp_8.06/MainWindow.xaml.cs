09b8e98577b423f645b450c2d27437d99f6658e6
Ходацька Аліна Віталіївна <khodatskaalina@gmail.com>
2024-06-08 18:51:06 +0300

    Ex 7 was done

--- WpfApp_8.06/MainWindow.xaml.cs
+++ WpfApp_8.06/MainWindow.xaml.cs
@@ -1,13 +1,6 @@
-﻿using System.Text;
+﻿using System;
+using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp_8._06
 {
@@ -16,6 +9,38 @@ namespace WpfApp_8._06
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private async void CalculatePower_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(NumberTextBox.Text, out double number) &&
+                double.TryParse(DegreeTextBox.Text, out double degree))
+            {
+                CalculatePowerButton.IsEnabled = false;
+
+                try
+                {
+                    double result = await Task.Run(() => CalculatePower(number, degree));
+                    ResultTextBlock.Text = $"Result: {result}";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"An error occurred: {ex.Message}");
+                }
+                finally
+                {
+                    CalculatePowerButton.IsEnabled = true;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numbers.");
+            }
+        }
+
+        private double CalculatePower(double number, double degree)
+        {
+            return Math.Pow(number, degree);
         }
     }
 }
